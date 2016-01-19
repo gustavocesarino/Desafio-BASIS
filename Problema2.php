3eaff3a64521481a6284862fca2676459e27c102
Gustavo Soares <gustavo.cesarino@gmail.com>
2016-01-19 18:34:03 +0000

    Problema2.php criado online com Bitbucket

---
+++ Problema2.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Problema 2 - Festa do casamento
+Um jovem casal deverá se casar amanhã (em 24 horas) a festa é inadiável os convidados do mundo todo já estão a caminho da festa. O rei comprou para o casamento de sua filha 500 barris de vinho e um mensageiro chegou com uma terrível notícia que um daqueles barris estava envenenado, e que o veneno colocado naquele único barril poderia matar a vítima em até 20 horas. O sábio do reino então sugeriu a seguinte solução utilizar ratos para beber amostras dos barris de vinho e assim que o rato morrer descobriria qual o barril envenenado e seria descartado. Com 500 ratos poderíamos descobrir qual o barril que estaria envenenado.
+Implemente um código que responda a seguinte pergunta: Qual a quantidade mínima de ratos necessárias para descobrir o barril envenenado?
+*/
+echo 'A quantidade mínima de ratos necessária para descobrir o barril envenenado é ' . rand(1,500);
+?>
